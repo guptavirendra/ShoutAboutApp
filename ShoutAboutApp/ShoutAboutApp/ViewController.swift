6c4c1b5d8612b2d67b7449f4bb88ecb917f8042f
VIRENDRA GUPTA <virendragupta@VIRENDRAs-MacBook-Pro.local>
2016-11-06 00:34:02 +0530

    login screen ui finished

--- ShoutAboutApp/ShoutAboutApp/ViewController.swift
+++ ShoutAboutApp/ShoutAboutApp/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  ShoutAboutApp
+//  ShoutAboutAppV
 //
 //  Created by VIRENDRA GUPTA on 05/11/16.
 //  Copyright © 2016 VIRENDRA GUPTA. All rights reserved.
@@ -11,17 +11,85 @@ import UIKit
 class ViewController: UIViewController
 {
 
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var mobileNumberTextField: UITextField!
+    var mobileNumberString:String = ""
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            mobileNumberTextField.addTarget(self, action:#selector(ViewController.edited), forControlEvents:UIControlEvents.EditingChanged)
+        submitButton.userInteractionEnabled = false
+        
     }
 
-    override func didReceiveMemoryWarning()
-    {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submitButtonClicked(sender: UIButton)
+    {
+        ///print hit webservice
+        print("Mobile Number to Submit \(mobileNumberString)")
+        mobileNumberTextField.resignFirstResponder()
+        
+        if NetworkConnectivity.isConnectedToNetwork() != true
+        {
+            displayAlertMessage("No Internet Connection")
+            
+        }else
+        {
+            //hit webservice
+        }
+        
+    }
+
+}
+
+
+extension ViewController:UITextFieldDelegate
+{
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool
+    {
+        var text:NSString =  textField.text ?? ""
+        text =  text.stringByReplacingCharactersInRange(range, withString: string)
+        
+        
+        if text.length == 10
+        {
+            submitButton.userInteractionEnabled = true
+        }else
+        {
+            submitButton.userInteractionEnabled = false
+        }
+        
+        if text.length > 10
+        {
+            return false
+        }
+        return true
+    }
+    func edited()
+    {
+        print("Edited \(mobileNumberTextField.text)")
+        mobileNumberString = mobileNumberTextField.text!
+    }
+}
+
+
+
+extension UIViewController
+{
+    /* Displays alert message
+     */
+    func displayAlertMessage(userMessage: String)
+    {
+        
+        let alert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        alert.addAction(okAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
 }
 
